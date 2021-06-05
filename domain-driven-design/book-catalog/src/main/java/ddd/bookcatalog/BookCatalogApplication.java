e125c76013ac870c2f4aec96afc578ba0acf2e2d
veronikaognjanovska <62549189+veronikaognjanovska@users.noreply.github.com>
2021-06-06 00:30:47 +0200

    Initial structure

---
+++ domain-driven-design/book-catalog/src/main/java/ddd/bookcatalog/BookCatalogApplication.java
@@ -0,0 +1,13 @@
+package ddd.bookcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookCatalogApplication.class, args);
+    }
+
+}
---
+++ domain-driven-design/shared-kernel/src/test/java/ddd/sharedkernel/SharedKernelApplicationTests.java
@@ -0,0 +1,13 @@
+package ddd.sharedkernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedKernelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
